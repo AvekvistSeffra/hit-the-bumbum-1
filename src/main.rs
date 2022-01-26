882877c832622887cfa866c64b0b1b9f8a28b178
Hampus Avekvist <hampus.avekvist@seffra.com>
2022-01-26 02:07:36 +0100

    Removed HelloPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 
-mod hello_plugin;
 mod ui_plugin;
 
-use hello_plugin::HelloPlugin;
 use ui_plugin::UiPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
-        .add_plugin(HelloPlugin)
         .add_plugin(UiPlugin)
         .run();
 }
