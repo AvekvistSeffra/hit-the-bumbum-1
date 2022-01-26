7283bdc7f288d188b4734aaa7bbbe29176341cda
Hampus Avekvist <hampus.avekvist@seffra.com>
2022-01-26 02:09:32 +0100

    Added UI state and toggleability

--- src/ui_plugin.rs
+++ src/ui_plugin.rs
@@ -1,17 +1,33 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContext };
 
-fn ui_example(egui_context: ResMut<EguiContext>) {
-    egui::Window::new("Hello").show(egui_context.ctx(), |ui| {
-        ui.label("world");
-    });
+#[derive(Default)]
+struct UiState {
+    show: bool,
+}
+
+fn ui_example(egui_context: ResMut<EguiContext>, ui_state: Res<UiState>) {
+    if ui_state.show {
+        egui::Window::new("Hello").show(egui_context.ctx(), |ui| {
+            ui.label("world");
+        });
+    }
+}
+
+fn ui_toggle(keys: Res<Input<KeyCode>>, mut ui_state: ResMut<UiState>) {
+    if keys.pressed(KeyCode::LControl)
+        && keys.pressed(KeyCode::LShift)
+        && keys.just_pressed(KeyCode::D)
+    {
+        ui_state.show = !ui_state.show;
+    }
 }
 
 pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
-        app
+        app.init_resource::<UiState>()
+            .add_system(ui_toggle)
             .add_system(ui_example);
     }
 }
